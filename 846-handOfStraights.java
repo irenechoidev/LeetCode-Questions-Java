4cb1c573d9c137150c2e6ee54d936982c33ebdcd
irenechoidev <143298470+irenechoidev@users.noreply.github.com>
2024-12-01 14:44:21 -0700

    Add files via upload

---
+++ 846-handOfStraights.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isNStraightHand(int[] hand, int groupSize) {
+        Map<Integer, Integer> map = new HashMap<>();
+        Arrays.sort(hand);
+        for (int num : hand) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        for (int num : hand) {
+
+            // If the count of a number num in the map is 0,
+            // it means all instances of this number have already been used in previous
+            // groups
+            if (map.getOrDefault(num, 0) == 0) {
+                continue;
+            }
+
+            // The condition ensures that
+            // each consecutive number required for the group exists in the map
+            for (int i = 0; i < groupSize; i++) {
+                if (map.getOrDefault(num + i, 0) == 0)
+                    return false;
+                map.put(num + i, map.getOrDefault(num + i, 0) - 1);
+            }
+        }
+        return true;
+    }
+}
