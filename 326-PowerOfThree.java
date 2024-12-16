6aa8eb4d06c35df781e4f3fa76c6bb16005de027
irenechoidev <143298470+irenechoidev@users.noreply.github.com>
2024-12-16 10:20:43 -0700

    Update 326-PowerOfThree.java

--- 326-PowerOfThree.java
+++ 326-PowerOfThree.java
@@ -1,7 +1,8 @@
 class Solution {
     public boolean isPowerOfThree(int n) {
         // if there exists an integer x such that n == 3^x(three to the power of x)
-
+        
+        // edge case:
         if (n < 1) return false;
 
         // 3 으로 나누어 지면
