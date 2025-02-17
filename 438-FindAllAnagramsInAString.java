0d01a2edd70bacacc85e44d34c95979da9f0ed75
irenechoidev <143298470+irenechoidev@users.noreply.github.com>
2025-02-16 21:27:59 -0700

    Add files via upload

---
+++ 438-FindAllAnagramsInAString.java
@@ -0,0 +1,42 @@
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+
+        List<Integer> result = new ArrayList<>();
+        int start = 0;
+        int end = 0;
+        int count = p.length();
+
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c : p.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        while (end < s.length()) {
+
+            char endChar = s.charAt(end);
+            if (map.containsKey(endChar)) {
+                map.put(endChar, map.get(endChar) - 1);
+                if (map.get(endChar) >= 0) {
+                    count -= 1;
+                }
+            }
+
+            while ((end - start + 1) == p.length()) {
+                if (count == 0)
+                    result.add(start);
+
+                char startChar = s.charAt(start);
+                if (map.containsKey(startChar)) {
+                    map.put(startChar, map.get(startChar) + 1);
+                    if (map.get(startChar) > 0) {
+                        count += 1;
+                    }
+                }
+                start += 1;
+            }
+
+            end += 1;
+        }
+        return result;
+    }
+}
