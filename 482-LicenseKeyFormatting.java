2e67d70dc36a450870006fdcf28d47b4a9fc0c6c
irenechoidev <143298470+irenechoidev@users.noreply.github.com>
2025-06-21 20:05:18 -0600

    Add files via upload

---
+++ 482-LicenseKeyFormatting.java
@@ -0,0 +1,32 @@
+class Solution {
+    public String licenseKeyFormatting(String s, int k) {
+        StringBuilder sb = new StringBuilder();
+
+        // Remove all dashes && Convert letters to uppercase
+        String str = reFormatStr(s);
+
+        // Iterate over the reformatted string backwards,
+        // adding a dash after every k characters.
+        int count = 0;
+        for (int i = str.length() - 1; i >= 0; i--) {
+            sb.append(str.charAt(i));
+            count += 1;
+            if (count == k && i > 0) {
+                sb.append('-');
+                count = 0;
+            }
+        }
+        return sb.reverse().toString();
+    }
+
+    // Remove all dashes && Convert letters to uppercase
+    private String reFormatStr(String s) {
+        StringBuilder sb = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            if (c != '-') {
+                sb.append(Character.toUpperCase(c));
+            }
+        }
+        return sb.toString();
+    }
+}
