bb447f02b3661c9799a5fd79964dde6f938c6ccc
irenechoidev <143298470+irenechoidev@users.noreply.github.com>
2025-11-07 19:27:04 -0700

    Add files via upload

---
+++ 1822-SignOfTheProductOfAnArray.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int arraySign(int[] nums) {
+        int count = 0;
+        for (int num : nums) {
+            if (num == 0)
+                return 0;
+            else if (num < 0)
+                count += 1;
+        }
+        return count % 2 == 0 ? 1 : -1;
+    }
+}
