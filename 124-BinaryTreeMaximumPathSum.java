11689a12f62121ffb3ff6731e8e5f6e38ebb42c2
irenechoidev <143298470+irenechoidev@users.noreply.github.com>
2025-12-13 20:14:40 -0700

    Add files via upload

---
+++ 124-BinaryTreeMaximumPathSum.java
@@ -0,0 +1,20 @@
+class Solution {
+    int max = Integer.MIN_VALUE;
+
+    public int maxPathSum(TreeNode root) {
+        dfs(root);
+        return max;
+    }
+
+    private int dfs(TreeNode root) {
+        if (root == null)
+            return 0;
+
+        int left = Math.max(0, dfs(root.left));
+        int right = Math.max(0, dfs(root.right));
+
+        max = Math.max(max, left + right + root.val);
+
+        return root.val + (Math.max(left, right));
+    }
+}
