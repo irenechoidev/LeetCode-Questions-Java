536e62a1bf5e671aab6ba2d0db2c8ba42d09055a
irenechoidev <143298470+irenechoidev@users.noreply.github.com>
2026-01-19 21:43:05 -0700

    Add files via upload

---
+++ 1046-LastStoneWeight.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
+        for (int stone : stones) {
+            pq.add(stone);
+        }
+
+        while (pq.size() > 1) {
+            int stone1 = pq.poll();
+            int stone2 = pq.poll();
+            pq.add(stone1 - stone2);
+        }
+        return pq.peek();
+    }
+}
