a64edbd1909164dc4f16132cf8ab8047516b1c80
irenechoidev <143298470+irenechoidev@users.noreply.github.com>
2026-02-01 15:19:22 -0700

    Add files via upload

---
+++ 102-BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null)
+            return res;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> temp = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode curr = queue.poll();
+                temp.add(curr.val);
+                if (curr.left != null)
+                    queue.add(curr.left);
+                if (curr.right != null)
+                    queue.add(curr.right);
+            }
+            res.add(temp);
+        }
+        return res;
+    }
+}
