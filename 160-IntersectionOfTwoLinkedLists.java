ab8941a3c2a3dd4c4eedf2dbf0c1bf424c3d0824
irenechoidev <143298470+irenechoidev@users.noreply.github.com>
2026-02-04 09:14:07 -0700

    Add files via upload

---
+++ 160-IntersectionOfTwoLinkedLists.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode aPointer = headA;
+        ListNode bPointer = headB;
+        while (aPointer != bPointer) {
+            if (aPointer == null) {
+                aPointer = headB;
+            } else {
+                aPointer = aPointer.next;
+            }
+            if (bPointer == null) {
+                bPointer = headA;
+            } else {
+                bPointer = bPointer.next;
+            }
+        }
+        return aPointer;
+    }
+}
