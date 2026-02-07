ba0082741703e892ecea8509873e96dafd8bb01f
irenechoidev <143298470+irenechoidev@users.noreply.github.com>
2026-02-07 16:04:17 -0700

    Add files via upload

---
+++ 1137-N-thTribonacciNumber.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int tribonacci(int n) {
+        if (n <= 1)
+            return n;
+        if (n == 2)
+            return 1;
+        int[] memo = new int[n + 1];
+        memo[0] = 0;
+        memo[1] = 1;
+        memo[2] = 1;
+
+        for (int i = 3; i < memo.length; i++) {
+            memo[i] = memo[i - 1] + memo[i - 2] + memo[i - 3];
+        }
+
+        return memo[n];
+    }
+}
