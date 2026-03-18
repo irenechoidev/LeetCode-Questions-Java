e2ccd29c6df99d0e1ac60fccba7c37ba4143927c
irenechoidev <143298470+irenechoidev@users.noreply.github.com>
2026-03-17 20:21:28 -0600

    Add files via upload

---
+++ 961-N-RepeatedElementsInSize2NArray.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int repeatedNTimes(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int n : nums) {
+            if (set.contains(n))
+                return n;
+            set.add(n);
+        }
+        return ' ';
+    }
+}
