894ffe09dbd86887459016ecb889540f99d71ac9
irenechoidev <143298470+irenechoidev@users.noreply.github.com>
2026-04-17 10:05:29 -0600

    Add files via upload

---
+++ 2574-LeftAndRightDifferences.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] leftRightDifference(int[] nums) {
+        int leftSum = 0;
+        int rightSum = 0;
+
+        for (int n : nums)
+            rightSum += n;
+
+        for (int i = 0; i < nums.length; i++) {
+            leftSum += nums[i];
+            rightSum -= nums[i];
+            nums[i] = Math.abs((leftSum - nums[i]) - rightSum);
+        }
+
+        return nums;
+    }
+}
