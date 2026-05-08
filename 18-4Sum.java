21568ee0b6026495b1202a698abb79ef6b605dd1
irenechoidev <143298470+irenechoidev@users.noreply.github.com>
2026-05-08 12:04:36 -0600

    Add files via upload

---
+++ 18-4Sum.java
@@ -0,0 +1,47 @@
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        int n = nums.length;
+        if (n < 4)
+            return res;
+
+        Arrays.sort(nums);
+
+        for (int i = 0; i < n - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+
+            if ((long) nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3] > target)
+                break;
+            if ((long) nums[i] + nums[n - 3] + nums[n - 2] + nums[n - 1] < target)
+                continue;
+
+            for (int j = i + 1; j < n - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1])
+                    continue;
+
+                int start = j + 1;
+                int end = n - 1;
+
+                while (start < end) {
+                    long sum = (long) nums[i] + nums[j] + nums[start] + nums[end];
+
+                    if (sum == target) {
+                        res.add(Arrays.asList(nums[i], nums[j], nums[start], nums[end]));
+                        while (start < end && nums[start] == nums[start + 1])
+                            start++;
+                        while (start < end && nums[end] == nums[end - 1])
+                            end--;
+                        start++;
+                        end--;
+                    } else if (sum > target) {
+                        end--;
+                    } else {
+                        start++;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+}
