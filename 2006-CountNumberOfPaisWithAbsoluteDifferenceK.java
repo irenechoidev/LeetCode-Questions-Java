9a6b7c44812a5378e5a1b8238a44568a9eabf280
irenechoidev <143298470+irenechoidev@users.noreply.github.com>
2026-05-12 17:06:00 -0600

    Add files via upload

---
+++ 2006-CountNumberOfPaisWithAbsoluteDifferenceK.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int countKDifference(int[] nums, int k) {
+        int res = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i; j < nums.length; j++) {
+                if (Math.abs(nums[i] - nums[j]) == k)
+                    res += 1;
+            }
+        }
+        return res;
+    }
+}
