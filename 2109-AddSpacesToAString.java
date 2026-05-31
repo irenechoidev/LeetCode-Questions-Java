44962381ec7de0db76182f34aaaa9b7a4bb0a446
irenechoidev <143298470+irenechoidev@users.noreply.github.com>
2026-05-31 14:51:34 -0600

    Add files via upload

---
+++ 2109-AddSpacesToAString.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String addSpaces(String s, int[] spaces) {
+        StringBuilder sb = new StringBuilder(s.length() + spaces.length);
+        int spaceIdx = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (spaceIdx < spaces.length && i == spaces[spaceIdx]) {
+                sb.append(' ');
+                spaceIdx += 1;
+            }
+            sb.append(s.charAt(i));
+        }
+        return sb.toString();
+
+    }
+}
