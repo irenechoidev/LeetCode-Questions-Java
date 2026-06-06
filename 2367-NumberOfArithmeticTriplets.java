fa6a9b6e9fb749d63c82512a07cd406f600bb2c8
irenechoidev <143298470+irenechoidev@users.noreply.github.com>
2026-06-05 21:08:53 -0600

    Add files via upload

---
+++ 2367-NumberOfArithmeticTriplets.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int arithmeticTriplets(int[] nums, int diff) {
+        int res = 0;
+        Set<Integer> set = new HashSet<>();
+        for (int n : nums) {
+            set.add(n);
+        }
+        for (int n : nums) {
+            if (set.contains(n + diff) && set.contains(n + (2 * diff))) {
+                res += 1;
+            }
+        }
+        return res;
+    }
+}
