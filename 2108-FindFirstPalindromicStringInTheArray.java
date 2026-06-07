cf90ca7800cbc30585456fad25ea2aa9c85cbf54
irenechoidev <143298470+irenechoidev@users.noreply.github.com>
2026-06-06 20:50:38 -0600

    Add files via upload

---
+++ 2108-FindFirstPalindromicStringInTheArray.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String firstPalindrome(String[] words) {
+        for (String word : words) {
+            if (isPalindrome(word))
+                return word;
+        }
+        return "";
+    }
+
+    private boolean isPalindrome(String s) {
+        int start = 0;
+        int end = s.length() - 1;
+        while (start < end) {
+            if (s.charAt(start) != s.charAt(end))
+                return false;
+            start += 1;
+            end -= 1;
+        }
+        return true;
+    }
+}
